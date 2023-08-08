f1a28db319838e11504c2dadf2056db90b98ce66
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-08 13:57:36 +0630

    Array lesson

---
+++ array.js
@@ -0,0 +1,7 @@
+let numbers = [
+    [1, 2, [7, 8,9]],
+    [4, 5, 6]
+]
+
+console.log(numbers[0][1])
+console.table(numbers);
--- number_string.js
+++
@@ -1,8 +0,0 @@
-let x = 20;
-let y = 10;
-let z = x + y;
-console.log(z);
-
-let userName = "Sai Aung Hlyan Htet";
-document.querySelector("#userName").innerHTML = userName;
-console.log(userName, typeof userName);
