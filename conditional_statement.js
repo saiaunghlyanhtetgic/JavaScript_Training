bbbffd04eaaab3492e4aed927e2d1cab90ddb3a5
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-08 14:43:03 +0630

    switch lesson

--- conditional_statement.js
+++
@@ -1,8 +0,0 @@
-let x = 30;
-let y = 39;
-
-if (x < y) {
-    console.log("x is smaller than y");
-} else {
-    console.log("x is greater than y");
-}
---
+++ switch.js
@@ -0,0 +1,8 @@
+let color = "green";
+
+switch (color) {
+    case "green": console.log("this is green"); break;
+    case "yellow": console.log("this is yellow"); break;
+    case "red": console.log("this is red"); break;
+    default: console.log("unknown color");
+}
