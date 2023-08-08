2b74931105c20b31708c1480524808a7e3f62dfa
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-08 14:31:29 +0630

    Conditional operators

--- array.js
+++
@@ -1,7 +0,0 @@
-let numbers = [
-    [1, 2, [7, 8,9]],
-    [4, 5, 6]
-]
-
-console.log(numbers[0][1])
-console.table(numbers);
---
+++ conditional_operators.js
@@ -0,0 +1,10 @@
+// Conditional operators
+// ===, ==, !=, <=, >=, >, <
+
+let x = 20;
+let y = "20";
+let z = x == y;
+console.log(z);
+
+z = x === y;
+console.log(z);
