ea10b8de1c830f83ecf0ce712c77e56a60e7d1b1
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-09 08:25:47 +0630

    Window obj

---
+++ window_obj.js
@@ -0,0 +1,6 @@
+// let name = prompt("what is your name?");
+// document.querySelector("#head").innerText = name;
+
+document.querySelector("#btn").addEventListener("click", () => {
+    window.location.href = "another.html";
+})
