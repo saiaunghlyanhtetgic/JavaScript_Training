2ef94f7c96fcaf92c84efb799218ccf5409e8e76
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-09 08:43:16 +0630

    dom

---
+++ dom.js
@@ -0,0 +1,3 @@
+console.log(window.document.all)
+console.log(document.links[1].innerText, document.links[0].className)
+console.log(document.scripts[0].src)
--- window_obj.js
+++
@@ -1,6 +0,0 @@
-// let name = prompt("what is your name?");
-// document.querySelector("#head").innerText = name;
-
-document.querySelector("#btn").addEventListener("click", () => {
-    window.location.href = "another.html";
-})
