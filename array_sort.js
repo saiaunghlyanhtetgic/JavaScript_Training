09a9d7ef5a6d984926c56624082225fa59c0cdd8
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-09 08:10:04 +0630

    string props and methods

--- array_sort.js
+++
@@ -1,5 +0,0 @@
-let numbers = [55, 54, 87, 33, 42, 1, 4, 90, 25];
-
-console.log(numbers.sort((a, b) => a - b));
-
-console.log(numbers.filter(num => num > 50));
---
+++ string_props.js
@@ -0,0 +1,10 @@
+let msg = "here we go on decembere 10\"th";
+let name = "fabrizione";
+
+msg = msg.concat(" - ", name);
+console.log(msg);
+console.log(msg.toUpperCase());
+console.log(msg.indexOf("w"));
+console.log(msg.substring(0, 4))
+console.log(msg.slice(-7));
+console.log(msg.includes("we"));
