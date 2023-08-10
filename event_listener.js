816ea7c67d258c464e025f4e578a0f4e865ca7b9
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-10 14:29:27 +0630

    form events

--- event_listener.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector("#btn1");
-btn.addEventListener("mousedown", (e) => {
-    alert(btn.innerText);
-    console.log(e);
-})
-
-let btn2 = document.querySelector("#btn2");
-
-btn2.onmouseup = (e) => { console.log(e.target);}
